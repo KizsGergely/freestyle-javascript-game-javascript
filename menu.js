1ad6b5f95a722f3bee094633ba79118d0c2a08a6
balintovics <molnar99b@gmail.com>
2021-03-04 10:29:57 +0100

    feat: SW crawl in 20 sec interval with theme music and cantina band music for welcome screen

--- menu.js
+++ menu.js
@@ -1,16 +1,20 @@
-function onKeyDown(space) {
+function menu(enter) {
+    const cantina = new Audio("static/sounds/cantina.mp3");
+    if (enter.keyCode === 32) {
+    cantina.play()
+    }
     let $crawl = document.querySelector(".star-wars");
     let $menupoints = document.querySelector(".menu");
     let $creators = document.getElementById('hide');
-    if (space.keyCode === 32) {
+    if (enter.keyCode === 13) {
         $menupoints.style.display="none";
         $creators.style.display="none";
+        const audio = new Audio("static/sounds/theme.mp3")
+        audio.play()
         $crawl.style.display="flex";
-    }
-    let $lastp = document.getElementById("lastp");
-    if ($lastp.style.position === '0'); {
-
+        setTimeout(function() {window.location.href = "http://localhost:63342/freestyle-javascript-game-javascript-KizsGergely/index.html";}, 18960);
     }
 }
 
-window.addEventListener("keydown", onKeyDown);
+window.addEventListener("keydown", menu);
+
